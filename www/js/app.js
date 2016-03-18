0cd0092e79b9e641829555f2aa697240a1ac60c8
Rick Bacci <rickbacci@gmail.com>
2016-03-18 13:39:36 -0400

    create TodoCtrl controller
    - remove code from blank template
    - add test data
    - create controller code
    - remove .run code

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,10 @@
 angular.module('todo', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
 });
-
-
